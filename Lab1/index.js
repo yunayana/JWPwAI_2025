e4477cc802af5afb6a2192427067ca1169e21eb9
Yana Trotsenko <21232@student.ans-elblag.pl>
2025-10-06 11:22:48 +0200

    path util v8

--- Lab1/index.js
+++ Lab1/index.js
@@ -1,12 +1,18 @@
 const http = require('http');
+const path = require("path");
+const util = require("util");
+const v8 = require("v8");
 const hostname = '127.0.0.1';
 const port = 8888;
-const server = http.createServer(function(request, response) {
+const server = http.createServer((request, response) => {
  response.statusCode = 200;
  response.setHeader('Content-Type', 'text/plain');
- response.end('Witaj, z serwera node.js !\n');
+ response.end('Hello, from node.js serwer !\n');
 
 });
 server.listen(port, hostname, () => {
+ console.log(v8.getHeapStatistics());
+ console.log(path.basename(__filename));
+ console.log(path.join(__dirname, 'uploads','images'));
  console.log(`Server running at http://${hostname}:${port}/`);
 });
