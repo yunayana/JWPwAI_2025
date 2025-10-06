6db20c5cb92a8707ab94af4a49201ff29c9aa964
Yana Trotsenko <21232@student.ans-elblag.pl>
2025-10-06 11:12:51 +0200

    Updated to exercise 2

--- Lab1/index.js
+++ Lab1/index.js
@@ -1,6 +1,7 @@
-const wait = 3000;
-console.log("Początek skryptu...");
-setTimeout(function(){
- console.log("Upłynęły minimum 3 sek...")
-}, wait);
-console.log("koniec skryptu...");
+const fs = require("fs");
+console.log("Początek skryptu.");
+fs.readFile('package.json', function (err, data) {
+ if (err) return console.error(err);
+ console.log(data.toString());
+});
+console.log("Koniec skryptu.");
