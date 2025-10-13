81ae97daf3ee2450472204ae9cebd6fecae7e48f
Yana Trotsenko <21232@student.ans-elblag.pl>
2025-10-13 10:50:57 +0200

    Modified all features with early termination option.

--- Lab2/index.js
+++ Lab2/index.js
@@ -7,31 +7,27 @@ const server = http.createServer(function (req, res) {
 
 function getTitles(res) {
     fs.readFile('./titles.json', function (err, data) {
-        if (err) {
-            hadError(err, res);
-        } else {
+        if (err) return hadError(err, res);
             getTemplate(JSON.parse(data.toString()), res);
-        }
     });
 }
 
 function getTemplate(titles, res) {
     fs.readFile('./template.html', function (err, data) {
-        if (err) {
-            hadError(err, res);
-        } else {
+        if (err) return hadError(err, res);
             formatHtml(titles, data.toString(), res);
-        }
     });
 }
 
 function formatHtml(titles, tmpl, res) {
-    let html = tmpl.replace('%', titles.join('</li><li>'));
+    if(!titles || !tmpl) return hadError(new Error('No data'), res);
+    const html = tmpl.replace('%', titles.join('</li><li>'));
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end(html);
 }
 
 function hadError(err, res) {
     console.error(err);
+    res.statusCode = 500;
     res.end('Error');
 }
