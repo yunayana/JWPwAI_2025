c335b99dd0cd2fbd553bb6858681ab33d92be529
Yana Trotsenko <21232@student.ans-elblag.pl>
2025-10-13 10:44:09 +0200

    testing code from ex.2

--- Lab2/index.js
+++ Lab2/index.js
@@ -1,25 +1,37 @@
 const http = require('http');
 const fs = require('fs');
-http.createServer(function(req, res) {
-    if (req.url == '/') {
-        fs.readFile('./titles.json', function(err, data) {
-            if (err) {
-                console.error(err);
-                res.end('titles.json error');
-            } else {
-                const titles = JSON.parse(data.toString());
-                fs.readFile('./template.html', function(err, data) {
-                    if (err) {
-                        console.error(err);
-                        res.end('template.html error');
-                    } else {
-                        const tmpl = data.toString();
-                        const html = tmpl.replace('%', titles.join('</li><li>'));
-                        res.writeHead(200, {'Content-Type': 'text/html'});
-                        res.end(html);
-                    }
-                });
-            }
-        });
-    }
+
+const server = http.createServer(function (req, res) {
+    getTitles(res);
 }).listen(8000, "127.0.0.1");
+
+function getTitles(res) {
+    fs.readFile('./titles.json', function (err, data) {
+        if (err) {
+            hadError(err, res);
+        } else {
+            getTemplate(JSON.parse(data.toString()), res);
+        }
+    });
+}
+
+function getTemplate(titles, res) {
+    fs.readFile('./template.html', function (err, data) {
+        if (err) {
+            hadError(err, res);
+        } else {
+            formatHtml(titles, data.toString(), res);
+        }
+    });
+}
+
+function formatHtml(titles, tmpl, res) {
+    let html = tmpl.replace('%', titles.join('</li><li>'));
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(html);
+}
+
+function hadError(err, res) {
+    console.error(err);
+    res.end('Error');
+}
